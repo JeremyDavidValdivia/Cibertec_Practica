2750c1597dc8b4b9d99c283aa1901281d07f31be
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-07-02 17:27:43 -0500

    Creando WebDeveloper.DataAccess

---
+++ WebDeveloper.DataAccess/WebContextDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WebDeveloper.Model;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebDeveloper.DataAccess
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb() : base("name=WebDeveloperConnectionString")
+        {
+            Database.SetInitializer(new WebDeveloperInitializer());
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /* Elimina Datos convencionales de EntityFramework */
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- WebDeveloper.Model/Products.cs
+++ WebDeveloper.Model/Products.cs
@@ -10,28 +10,40 @@ namespace WebDeveloper.Model
         [Key]
         public int ProductID { get; set; }
 
-        [Required]
+        [Display(Name = "Product Name : ")]
+        [Required(ErrorMessage = "Product Name is required")]
         [StringLength(40)]
         public string ProductName { get; set; }
 
+        [Display(Name = "Supplier ID : ")]
         public int? SupplierID { get; set; }
 
+        [Display(Name = "Category ID : ")]
         public int? CategoryID { get; set; }
 
+        [Display(Name = "Quantity Per Unit : ")]
+        [Required(ErrorMessage = "Quantity Per Unit is required")]
         [StringLength(20)]
         public string QuantityPerUnit { get; set; }
 
+        [Display(Name = "Unit Price : ")]
         [Column(TypeName = "money")]
         public decimal? UnitPrice { get; set; }
 
+        [Display(Name = "Units In Stock : ")]
         public short? UnitsInStock { get; set; }
 
+        [Display(Name = "Units On Order : ")]
         public short? UnitsOnOrder { get; set; }
 
+        [Display(Name = "Reorder Level : ")]
         public short? ReorderLevel { get; set; }
 
+        [Display(Name = "Discontinued : ")]
+        [Required(ErrorMessage = "Discontinued is required")]
         public bool Discontinued { get; set; }
 
+        [Display(Name = "Categories : ")]
         public virtual Categories Categories { get; set; }
     }
 }
